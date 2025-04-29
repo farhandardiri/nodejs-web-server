7624615bbace638ffd5b5d61b3b6e2e480bebecf
farhandardiri <faran.f4124n@gmail.com>
2025-04-29 12:47:36 +0700

    add routing

---
+++ routes.js
@@ -0,0 +1,33 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+];
+
+module.exports = routes;
