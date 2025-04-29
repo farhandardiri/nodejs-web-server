661279d5783d32e6561caedf92a168da587a59c6
farhandardiri <faran.f4124n@gmail.com>
2025-04-29 11:19:11 +0700

    request body

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const http = require("http");
 
 const requetsListerner = (request, response) => {
   response.setHeader("Content-Type", "text/html");
-
   response.statusCode = 200;
-  response.end("<h1>hello world server</h1>");
+
+  const { method } = request;
+
+  if (method === "GET") {
+    response.end("<h1>GET request</h1>");
+  }
+  if (method === "POST") {
+    let body = [];
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    request.on("end", () => {
+      body = Buffer.concat(body).toString();
+      const { name } = JSON.parse(body);
+      response.end(`<h1>Hai, ${name}!</h1>`);
+    });
+  }
+  if (method === "PUT") {
+    response.end("<h1>PUT request</h1>");
+  }
+  if (method === "DELETE") {
+    response.end("<h1>DELETE request</h1>");
+  }
+
+  //   response.end("<h1>hello world server</h1>");
 };
 
 const server = http.createServer(requetsListerner);
 
-const port = 3000;
+const port = 5000;
 const host = "localhost";
 
 server.listen(port, host, () => {
