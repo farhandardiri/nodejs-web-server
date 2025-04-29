d9fff9b8f6c9d8ba99eb1da78e0bda46f8567a2c
farhandardiri <faran.f4124n@gmail.com>
2025-04-29 12:27:51 +0700

    i hapi

--- server.js
+++ server.js
@@ -1,40 +1,20 @@
-const http = require("http");
+"use strict";
 
-const requetsListerner = (request, response) => {
-  response.setHeader("Content-Type", "text/html");
-  response.statusCode = 200;
+const Hapi = require("@hapi/hapi");
 
-  const { method } = request;
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
 
-  if (method === "GET") {
-    response.end("<h1>GET request</h1>");
-  }
-  if (method === "POST") {
-    let body = [];
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h1>`);
-    });
-  }
-  if (method === "PUT") {
-    response.end("<h1>PUT request</h1>");
-  }
-  if (method === "DELETE") {
-    response.end("<h1>DELETE request</h1>");
-  }
-
-  //   response.end("<h1>hello world server</h1>");
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-const server = http.createServer(requetsListerner);
-
-const port = 5000;
-const host = "localhost";
-
-server.listen(port, host, () => {
-  console.log(`server is running on http://${host}:${port}`);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
+
+init();
